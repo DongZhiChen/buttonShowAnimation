e05f3fa16a61e0ab3b8f2b6b0610d58abc5f7759
陈东芝 <chendongzhi@chendongzhideMacBook-Air.local>
2016-08-06 18:53:16 +0800

    none thing

---
+++ buttonShowAnimation/V_ShowShare.h
@@ -0,0 +1,13 @@
+//
+//  V_ShowShare.h
+//  buttonShowAnimation
+//
+//  Created by 陈东芝 on 16/8/6.
+//  Copyright © 2016年 ChenDongZhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface V_ShowShare : UIView
+
+@end
---
+++ buttonShowAnimation/V_ShowShare.m
@@ -0,0 +1,21 @@
+//
+//  V_ShowShare.m
+//  buttonShowAnimation
+//
+//  Created by 陈东芝 on 16/8/6.
+//  Copyright © 2016年 ChenDongZhi. All rights reserved.
+//
+
+#import "V_ShowShare.h"
+
+@implementation V_ShowShare
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
